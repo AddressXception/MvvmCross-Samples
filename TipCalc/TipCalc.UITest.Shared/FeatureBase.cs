91122fb8dd1da036ae65ecb7a9009d6a70b26231
Matt Wilhelm <matt.wilhelm@infernored.com>
2016-03-19 15:25:34 -0400

    Add FeatureBase.cs
    Add an abstract base class

---
+++ TipCalc/TipCalc.UITest.Shared/FeatureBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Xamarin.UITest;
+
+namespace TipCalc.UITest.Shared
+{
+    public abstract class FeatureBase
+    {
+        protected IApp App;
+
+        protected FeatureBase()
+        {
+
+        }
+
+        protected abstract void CreateApp(bool reset = true);
+
+        protected abstract void RegisterScreens();
+
+        //The SpecFlow's Aspects are called as follows:
+        // BeforeTestRun -> BeforeFeature -> BeforeScenario -> BeforeScenarioBlock -> BeforeStep
+        // AfterStep <- AfterScenarioBlock <- AfterScenario <- AfterFeature <- AfterTestRun
+
+        /// <summary>
+        /// automation logic that has to run before
+        /// the entire test run
+        /// </summary>
+        [BeforeTestRun()]
+        public static void BeforeTestRun()
+        {
+            Console.WriteLine("BeforeTestRun_Called");
+        }
+
+        /// <summary>
+        /// automation logic that has to run before
+        /// executing each feature
+        /// </summary>
+        [BeforeFeature()]
+        public static void BeforeFeature()
+        {
+            Console.WriteLine("BeforeFeature_Called");
+        }
+
+        /// <summary>
+        /// automation logic that has to run after 
+        /// executing each feature
+        /// </summary>
+        [AfterFeature()]
+        public static void AfterFeature()
+        {
+            Console.WriteLine ("AfterFeature_Called");
+        }
+
+        /// <summary>
+        /// automation logic that has to run after 
+        /// the entire test run
+        /// </summary>
+        [AfterTestRun()]
+        public static void AfterTestRun()
+        {
+            Console.WriteLine ("AfterTestRun_Called");
+        }
+    }
+}
