60397a9a914d63a3ee93a87fbf0416c5d279a8b3
Matt Wilhelm <matt.wilhelm@infernored.com>
2016-03-19 15:51:56 -0400

    Add Custom Assert
    Adding custom assert

---
+++ TipCalc/TipCalc.UITest.Shared/Common/Assert.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TipCalc.UITest.Shared.Common
+{
+    public static class Assert
+    {
+        internal static EventHandler<EventArgs> AssertionFailure;
+
+        public static void IsTrue(bool condition, string message = null)
+        {
+            if (condition)
+                return;
+            Assert.HandleFailure("Assert.IsTrue", message);
+        }
+
+        public static void IsFalse(bool condition, string message = null)
+        {
+            if (!condition)
+                return;
+            Assert.HandleFailure("Assert.IsFalse", message);
+        }
+
+        public static void AreEqual<T>(T expected, T actual, string message = null)
+        {
+            if (object.Equals((object)expected, (object)actual))
+                return;
+            Assert.HandleFailure("Assert.AreEqual", message);
+        }
+
+        public static void Inconclusive()
+        {
+
+        }
+
+        internal static void HandleFailure(string assertionName, string message)
+        {
+            if (Assert.AssertionFailure != null)
+                Assert.AssertionFailure((object)null, EventArgs.Empty);
+            throw new AssertFailedException(message);
+        }
+    }
+}
---
+++ TipCalc/TipCalc.UITest.Shared/Common/AssertFailedException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TipCalc.UITest.Shared.Common
+{
+    [Serializable]
+    public class AssertFailedException : Exception
+    {
+        public AssertFailedException()
+        {
+        }
+
+        public AssertFailedException(string message) : this(message, (Exception)null)
+        {
+
+        }
+
+        public AssertFailedException(string message, Exception innerException) : base(message, innerException)
+        {
+            Console.WriteLine("Assert Failed: " + message + "Inner Exception: " + innerException);
+        }
+
+        public AssertFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            Console.WriteLine("Assert Failed");
+        }
+
+    }
+}
