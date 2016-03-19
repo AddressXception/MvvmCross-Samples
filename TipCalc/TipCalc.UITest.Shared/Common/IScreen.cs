0961f6335dbfbbd9891a476cf41104ef3e5dad6c
Matt Wilhelm <matt.wilhelm@infernored.com>
2016-03-19 15:39:25 -0400

    Add IScreen.cs
    Add a screen interface to specify a common "FriendlyName" property
    between different screens.

---
+++ TipCalc/TipCalc.UITest.Shared/Common/IScreen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalc.UITest.Shared.Common
+{
+    public interface IScreen
+    {
+        /// <summary>
+        /// The friendly name is used to identify the screen in Gherkin tests
+        /// </summary>
+        string FriendlyName { get; }
+    }
+}
