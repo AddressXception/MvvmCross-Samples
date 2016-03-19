1f89049366f1c56800f21aa89efd9a2a2cce9746
Matt Wilhelm <matt.wilhelm@infernored.com>
2016-03-19 16:48:59 -0400

    Add GeneratorProvider & provider plugin classes

--- TipCalc/TipCalc.UITest.Windows.Generator/WindowsCodedUIGeneratorProvider.cs
+++ TipCalc/TipCalc.UITest.Windows.Generator/WindowsCodedUIGeneratorProvider.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.CodeDom;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Utils;
+
+// ReSharper disable InconsistentNaming
 
 namespace TipCalc.UITest.Windows.Generator
 {
-    public class WindowsCodedUIGeneratorProvider
+    /// <summary>
+    /// from: https://github.com/techtalk/SpecFlow/wiki/Using-SpecFlow-with-CodedUI-API
+    /// </summary>
+    public class WindowsCodedUIGeneratorProvider : MsTest2010GeneratorProvider
     {
+        public WindowsCodedUIGeneratorProvider(CodeDomHelper codeDomHelper)
+            : base(codeDomHelper)
+        {
+        }
+
+        public override void SetTestClass(TechTalk.SpecFlow.Generator.TestClassGenerationContext generationContext, string featureTitle, string featureDescription)
+        {
+            base.SetTestClass(generationContext, featureTitle, featureDescription);
+
+            foreach (CodeAttributeDeclaration customAttribute in generationContext.TestClass.CustomAttributes)
+            {
+                if (customAttribute.Name == "Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute")
+                {
+                    generationContext.TestClass.CustomAttributes.Remove(customAttribute);
+                    break;
+                }
+            }
+
+            generationContext.TestClass.CustomAttributes.Add(new CodeAttributeDeclaration(new CodeTypeReference("Microsoft.VisualStudio.TestTools.UITesting.CodedUITestAttribute")));
+        }
     }
 }
---
+++ TipCalc/TipCalc.UITest.Windows.Generator/WindowsCodedUIProviderPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BoDi;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+
+namespace TipCalc.UITest.Windows.Generator
+{
+    public class WindowsCodedUIProviderPlugin : IGeneratorPlugin
+    {
+        public void RegisterDependencies(ObjectContainer container)
+        {
+
+        }
+
+        public void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            var testProviderName = generatorConfiguration.GeneratorConfiguration.GeneratorUnitTestProvider;
+            if (testProviderName.Equals("mstest", StringComparison.InvariantCultureIgnoreCase) ||
+                    testProviderName.Equals("mstest.2010", StringComparison.InvariantCultureIgnoreCase))
+            {
+                container.RegisterTypeAs<WindowsCodedUIGeneratorProvider, IUnitTestGeneratorProvider>();
+            }
+        }
+
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+
+        }
+    }
+}
