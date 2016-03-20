1a0b458e99d447590af2c01802283694f4583199
Matt Wilhelm <matt.wilhelm@infernored.com>
2016-03-20 12:25:41 -0400

    Create Common Xamarin classes

---
+++ TipCalc/TipCalc.UITest.Xamarin/Common/InstrumentsRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalc.UITest.Xamarin.Common
+{
+    /// <summary>
+    /// Based on: https://github.com/RobGibbens/BddWithXamarinUITest/blob/master/InstrumentsRunner.cs
+    /// </summary>
+    class InstrumentsRunner
+    {
+    }
+}
