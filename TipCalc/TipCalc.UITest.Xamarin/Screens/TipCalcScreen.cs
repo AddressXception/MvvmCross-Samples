33ee6e0b63eb480e85e642f4b5174a88ab18050c
Matt Wilhelm <matt.wilhelm@infernored.com>
2016-03-27 12:24:10 -0400

    Add Xamarin TipCalcScreen

---
+++ TipCalc/TipCalc.UITest.Xamarin/Screens/TipCalcScreen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalc.UITest.Shared.Screens;
+
+namespace TipCalc.UITest.Xamarin.Screens
+{
+    public class TipCalcScreenAndroid : ITipCalcScreen
+    {
+        public string FriendlyName => "Tip";
+        public string SubTotalEntry => "subTotal_EditText";
+        public string TipAmount => "tipAmount_TextView";
+    }
+
+    public class TipCalcScreenIOS : ITipCalcScreen
+    {
+        public string FriendlyName => "Tip";
+        public string SubTotalEntry => "SubTotalTextField";
+        public string TipAmount => "TipLabel";
+    }
+}
